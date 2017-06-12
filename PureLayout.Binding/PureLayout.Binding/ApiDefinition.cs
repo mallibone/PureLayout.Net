67ae4cd8433de58b38fc70e8023a81de80510974
Mark Allibone <mallibone@outlook.com>
2017-06-12 19:59:23 +0200

    Refactoring Names of mapped classes

--- PureLayout.Binding/PureLayout.Binding/ApiDefinition.cs
+++ PureLayout.Binding/PureLayout.Binding/ApiDefinition.cs
@@ -5,83 +5,13 @@ using UIKit;
 
 namespace PureLayout.Net
 {
-	// The first step to creating a binding is to add your native library ("libNativeLibrary.a")
-	// to the project by right-clicking (or Control-clicking) the folder containing this source
-	// file and clicking "Add files..." and then simply select the native library (or libraries)
-	// that you want to bind.
-	//
-	// When you do that, you'll notice that MonoDevelop generates a code-behind file for each
-	// native library which will contain a [LinkWith] attribute. MonoDevelop auto-detects the
-	// architectures that the native library supports and fills in that information for you,
-	// however, it cannot auto-detect any Frameworks or other system libraries that the
-	// native library may depend on, so you'll need to fill in that information yourself.
-	//
-	// Once you've done that, you're ready to move on to binding the API...
-	//
-	//
-	// Here is where you'd define your API definition for the native Objective-C library.
-	//
-	// For example, to bind the following Objective-C class:
-	//
-	//     @interface Widget : NSObject {
-	//     }
-	//
-	// The C# binding would look like this:
-	//
-	//     [BaseType (typeof (NSObject))]
-	//     interface Widget {
-	//     }
-	//
-	// To bind Objective-C properties, such as:
-	//
-	//     @property (nonatomic, readwrite, assign) CGPoint center;
-	//
-	// You would add a property definition in the C# interface like so:
-	//
-	//     [Export ("center")]
-	//     CGPoint Center { get; set; }
-	//
-	// To bind an Objective-C method, such as:
-	//
-	//     -(void) doSomething:(NSObject *)object atIndex:(NSInteger)index;
-	//
-	// You would add a method definition to the C# interface like so:
-	//
-	//     [Export ("doSomething:atIndex:")]
-	//     void DoSomething (NSObject object, int index);
-	//
-	// Objective-C "constructors" such as:
-	//
-	//     -(id)initWithElmo:(ElmoMuppet *)elmo;
-	//
-	// Can be bound as:
-	//
-	//     [Export ("initWithElmo:")]
-	//     IntPtr Constructor (ElmoMuppet elmo);
-	//
-	// For more information, see http://developer.xamarin.com/guides/ios/advanced_topics/binding_objective-c/
-	//
-
-	//[Static]
-	//[Verify(ConstantsInterfaceAssociation)]
-	//partial interface Constants
-	//{
-	//	// extern double PureLayoutVersionNumber;
-	//	[Field("PureLayoutVersionNumber", "__Internal")]
-	//	double PureLayoutVersionNumber { get; }
-
-	//	// extern const unsigned char [] PureLayoutVersionString;
-	//	[Field("PureLayoutVersionString", "__Internal")]
-	//	byte[] PureLayoutVersionString { get; }
-	//}
-
 	// typedef void (^ALConstraintsBlock)();
 	delegate void ALConstraintsBlock();
 
 	// @interface PureLayout (UIView)
 	[Category]
 	[BaseType(typeof(UIView))]
-	interface UIView_PureLayout
+	interface UIViewPureLayout
 	{
 		// +(instancetype _Nonnull)newAutoLayoutView;
 		[Static]
@@ -256,7 +186,7 @@ namespace PureLayout.Net
 	// @interface PureLayout (NSArray)
 	[Category]
 	[BaseType(typeof(NSArray))]
-	interface NSArray_PureLayout
+	interface NSArrayPureLayout
 	{
 		// -(void)autoInstallConstraints;
 		[Export("autoInstallConstraints")]
@@ -314,7 +244,7 @@ namespace PureLayout.Net
 	// @interface PureLayout (NSLayoutConstraint)
 	[Category]
 	[BaseType(typeof(NSLayoutConstraint))]
-	interface NSLayoutConstraint_PureLayout
+	interface NSLayoutConstraintPureLayout
 	{
 		// +(NSArray<NSLayoutConstraint *> * _Nonnull)autoCreateAndInstallConstraints:(ALConstraintsBlock _Nonnull)block;
 		[Static]
