64752b5881d42ad8a8b5fae9da811350cbb14072
Andrew Olson <andrew@olsononline.org>
2020-09-17 14:17:09 -0500

    sample mac app

---
+++ PureLayoutSample/PureLayoutSample.Mac/ViewController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+using PureLayout.Net;
+
+namespace PureLayoutSample.Mac
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+
+            var button = new NSButton();
+            View.AddSubview(button);
+
+            button.AutoPinEdgesToSuperviewEdgesWithInsets(new NSEdgeInsets(50, 50, 50, 50));
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
